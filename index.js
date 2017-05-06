11c93650a1a08b06736e8c2c31ce1dae696c39dc
Tomas Muñoz Testón <tomini18@hotmail.com>
2017-05-06 18:17:30 +0200

    Modify plugin bars Kibana.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function(kibana) {
 	return new kibana.Plugin({
 		uiExports: {
-			visTypes: ['plugins/plugin-eskid3/plugin-eskid3']
+			visTypes: ['plugins/plugin-eskid3-exampleBars/plugin-eskid3-bars']
 		}
 	});
 };
